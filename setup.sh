a3d520db882e82f546d3c914f9a00778a59e225d
Ginsburg <ginsburgnm@ornl.gov>
2018-07-12 14:48:40 -0400

    cleaner python installation, though had to do a couple weird echos

--- setup.sh
+++ setup.sh
@@ -21,6 +21,45 @@ function invalid(){
     exit 1
 }
 
+function pyinstall(){
+    #method can be root(0), user(1), or virtualenv(2)
+    method=$1
+    package=$2
+
+    if [[ "$method" == "2" ]]; then
+        python -c "import virtualenv" 2>/dev/null
+        if [[ "$?" == "1" ]]; then
+            read -rp "virtualenv package not detected would you like to install it? (y/n): " installenv
+            installenv="$(echo "$installenv" | tr '[:upper:]' '[:lower:]')"
+
+            if [[ "$installenv" == "y" ]]; then
+                read -rp "Would you like to install virtualenv as root(0) or user(1)?" vinstall
+                pyinstall "$vinstall" "virtualenv"
+            elif [[ "$installenv" == "n" ]]; then
+                read -rp "how would you like to install the python requirements instead? root(0)/user(1): " preq
+                pyinstall "$preq" "$package"
+                return 0
+            else
+                invalid
+            fi
+        fi 
+        read -rp "What would you like to call the virtualenv? (Default pkpass): " venv
+        if [ -z "$venv" ]; then
+            venv="pkpass"
+        fi
+        python -m virtualenv "$venv"
+        source "$venv"/bin/activate
+        pip install -r requirements.txt
+        
+    elif [[ "$method" == "0" ]]; then
+        sudo python -m pip install $( echo "$package")
+    elif [[ "$method" == "1" ]]; then
+        python -m pip install $(echo "$package") --user
+    else
+        invalid
+    fi
+}
+
 home="$HOME"/passdb
 echo "If not using defaults for the following paths please use full filepath"
 echo "Or relative to home using ~"
@@ -52,60 +91,7 @@ pwstore: $pwstore" > .pkpassrc
 
 read -rp "Would you like to install the python requirements as root(0),user(1),or venv(2)?" pinstall
 
-case "$pinstall" in
-"0")
-    sudo python -m pip install -r requirements.txt
-    ;;
-"1")
-    python -m pip install -r requirements.txt --user
-    ;;
-"2")
-    #virtualenv install
-    #check to see if virtualenv package exists return 0 is true
-    python -c "import virtualenv" 2>/dev/null
-
-    if [ "$?" == "1" ]; then
-        read -rp "virtualenv package not dected would you like to install it?(y/n)" installvenv
-        #make user input lower case
-        installvenv="$(echo "$installvenv" | tr '[:upper:]' '[:lower:]')"
-
-        if [[ "$installvenv" == "y" ]]; then
-            read -rp "Would you like to install virtualenv as root(0) or user(1)?" vinstall
-            if [[ "$vinstall" == "0" ]]; then
-                sudo python -m pip install virtualenv
-            elif [[ "$vinstall" == "1" ]]; then
-                python -m pip install virtualenv --user
-            else
-                invalid
-            fi
-
-        elif [[ "$installvenv" == "n" ]]; then
-            read -rp "how would you like to install the python requirements instead? root(0)/user(1): " preq
-            if [[ "$preq" == 0 ]]; then
-                sudo python -m pip install -r requirements.txt
-            elif [[ "$preq" == 1 ]]; then
-                python -m pip install -r requirements.txt --user
-            fi
-
-        else
-            invalid
-        fi
-    fi
-    #don't process virtualenv install if user says no otherwise do it
-    if [[ "$installvenv" != "n" ]]; then
-        read -rp "What would you like to call the venv? (Default pkpass): " venv
-        if [ -z "$venv" ];then
-            venv="pkpass"
-        fi
-
-        python -m virtualenv "$venv"
-        source "$venv"/bin/activate
-        pip install -r requirements.txt
-    fi
-    ;;
-*)
-    invalid
-esac
+pyinstall "$pinstall" '-r requirements.txt'
 
 echo "testing versions of openssl and pkcs15-tool"
 echo "if version numbers return you're probably good"
