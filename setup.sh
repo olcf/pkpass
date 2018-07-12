65fe4c04a30074f15454937775efd6e555cbf374
Ginsburg <ginsburgnm@ornl.gov>
2018-07-12 14:55:25 -0400

    missed a requirements

--- setup.sh
+++ setup.sh
@@ -82,7 +82,7 @@ function pyinstall(){
         fi
         python -m virtualenv "$venv"
         source "$venv"/bin/activate
-        pip install -r requirements.txt
+        pip install $( echo "$package")
         
     elif [[ "$method" == "0" ]]; then
         sudo python -m pip install $( echo "$package")
