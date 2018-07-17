db5d15b20e415e58d69755a6ce10e70c29c863de
Ginsburg <ginsburgnm@ornl.gov>
2018-07-17 10:50:35 -0400

    adding check to see if pip is installed

--- setup.sh
+++ setup.sh
@@ -9,11 +9,12 @@ function pyinstall(){
     #method can be root(0), user(1), or virtualenv(2)
     method=$1
     local package=$2
-
     IFS=' ' 
+
     read -r -a package <<< "${package[@]}"
     if [[ "$method" == "2" ]]; then
         python -c "import virtualenv" 2>/dev/null
+
         if [[ "$?" == "1" ]]; then
             read -rp "virtualenv package not detected would you like to install it? (y/n): " installenv
             installenv="$(echo "$installenv" | tr '[:upper:]' '[:lower:]')"
@@ -21,16 +22,15 @@ function pyinstall(){
             if [[ "$installenv" == "y" ]]; then
                 read -rp "Would you like to install virtualenv as root(0) or user(1)?" vinstall
                 pyinstall "$vinstall" "virtualenv"
-
             elif [[ "$installenv" == "n" ]]; then
                 read -rp "how would you like to install the python requirements instead? root(0)/user(1): " preq
                 pyinstall "$preq" "${package[@]}"
                 return 0
-
             else
                 invalid
             fi
         fi 
+
         read -rp "What would you like to call the virtualenv? (Default pkpass): " venv
 
         if [ -z "${venv}" ]; then
@@ -60,6 +60,7 @@ fi
 home="$HOME"/passdb
 echo "If not using defaults for the following paths please use full filepath"
 echo "Or relative to home using ~"
+
 read -rp "Directory for certpath (defaults to ~/passdb/certs): " certpath
 
 certpath="${certpath:-${home}/certs}"
@@ -84,12 +85,12 @@ read -rp "Directory for password store (defaults to ~/passdb/passwords): " pwsto
 pwstore="${pwstore:-${home}/passwords}"
 pwstore="${pwstore/#\~/$HOME}"
 mkdir -p "${pwstore}"
-
 pkcs11-tool -L
 
 read -rp "Available slots listed above, which would you like to use? (defaults to 0): " cardslot
 
 cardslot="${cardslot:-0}"
+
 echo -e "certpath: $certpath 
 keypath: $keypath
 cabundle: $cabundle
@@ -97,7 +98,9 @@ pwstore: $pwstore
 default_card: $cardslot" > .pkpassrc
  
 read -rp "Would you like to install the python requirements as root(0),user(1),or venv(2)?" pinstall
+
 pyinstall "$pinstall" '-r requirements.txt'
+
 echo "testing versions of openssl and pkcs15-tool"
 echo "if version numbers return you're probably good"
 echo "for sanity sake Noah's return values were: "
