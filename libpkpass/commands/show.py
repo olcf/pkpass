898d2ecd7640d2b77aa1866d0f8a3f5b47b343fa
nginsburg <ginsburgnm@ornl.gov>
2019-01-23 17:06:39 -0500

    stupid freaking bug

--- libpkpass/commands/show.py
+++ libpkpass/commands/show.py
@@ -10,7 +10,7 @@ class Show(Command):
     the CLI to show passwords that have been distributed to users"""
     name = 'show'
     description = 'Display a password'
-    selected_args = ['pwname', 'pwstore', 'stdin', 'identity', 'certpath', 'keypath'
+    selected_args = ['pwname', 'pwstore', 'stdin', 'identity', 'certpath', 'keypath',
                      'cabundle', 'nopassphrase', 'noverify', 'card_slot', 'all']
 
     def _run_command_execution(self):
